04a8f130896f93c4b3fc1558da45f88e6eb43a7f
Andreas Imboden <andreas.imboden@bossinfo.ch>
2024-06-19 14:59:05 +0200

    Add init-profile.ps1

---
+++ init-profile.ps1
@@ -0,0 +1,6 @@
+if (-Not (Test-Path $profile))
+{
+	Invoke-WebRequest -Uri https://raw.githubusercontent.com/anim-bossinfo-ch/PowerShell/main/template-profile.ps1 -OutFile $profile
+	
+	. $profile
+}
