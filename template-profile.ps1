ff0dbb3090aa5696d74c97594b60607e6403ff7c
Andreas Imboden <andreas.imboden@bossinfo.ch>
2024-06-19 15:26:57 +0200

    x

--- template-profile.ps1
+++ template-profile.ps1
@@ -2,13 +2,14 @@ function CloneAndPullAndDotSourceScript($targetDir, $cloneUrl, $repoName) {
     if (-Not (Test-Path $targetDir -PathType Container))
     {
         New-Item -ItemType Directory -Force -Path $targetDir
-        git -C $targetDir clone $cloneUrl
+        git -C $targetDir clone $cloneUrl | out-null
     }
 
     $repoPath = Join-Path -Path $targetDir -ChildPath $repoName
     git -C $repoPath pull | out-null
-    Write-Host $repoPath\Microsoft.PowerShell_profile.ps1
-    return "$repoPath\Microsoft.PowerShell_profile.ps1"
+	$customProfile = $repoPath\Microsoft.PowerShell_profile.ps1
+    Write-Host $customProfile
+    return $customProfile
 }
 
 $targetDir = 'C:\Admin\freaxnx01'
