24ce2c4892fe522b0a696255e3a1480d5c343c21
anim-bossinfo-ch <163823909+anim-bossinfo-ch@users.noreply.github.com>
2024-06-20 08:34:00 +0200

    Update init-profile.ps1

--- init-profile.ps1
+++ init-profile.ps1
@@ -1,6 +1,4 @@
 if (-Not (Test-Path $profile))
 {
 	Invoke-WebRequest -Uri https://raw.githubusercontent.com/anim-bossinfo-ch/PowerShell/main/template-profile.ps1 -OutFile $profile
-	
-	. $profile
 }
